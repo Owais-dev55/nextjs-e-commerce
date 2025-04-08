0b3926d972780a06fb48181ad325148f65fae831
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2025-04-08 19:56:35 +0500

    Updated page.tsx

--- src/app/(LoginForm)/page.tsx
+++ src/app/(LoginForm)/page.tsx
@@ -1,8 +1,10 @@
 "use client"
+
 import React from 'react'
 import SignIn from './signIn/SignIn'
 import Navbar from '@/Component/Navbar/Navbar'
 
+export const dynamic = 'force-dynamic'
 const page = () => {
   return (
     <>
